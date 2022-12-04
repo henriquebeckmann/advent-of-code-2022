e530c22d791d3e8e740895e35185d871788b2af7
Henrique Beckmann <henriquebeckmann2@gmail.com>
2022-12-03 21:18:30 -0300

    editing target folder in gitignore

--- day01b/target/debug/day01b.d
+++
@@ -1,1 +0,0 @@
-/home/henrique/dev/git/advent-of-code-2022/day01b/target/debug/day01b: /home/henrique/dev/git/advent-of-code-2022/day01b/input.txt /home/henrique/dev/git/advent-of-code-2022/day01b/src/main.rs
--- day01b/target/debug/deps/day01b-c6f3e98312491d31.d
+++
@@ -1,6 +0,0 @@
-/home/henrique/dev/git/advent-of-code-2022/day01b/target/debug/deps/day01b-c6f3e98312491d31: src/main.rs src/../input.txt
-
-/home/henrique/dev/git/advent-of-code-2022/day01b/target/debug/deps/day01b-c6f3e98312491d31.d: src/main.rs src/../input.txt
-
-src/main.rs:
-src/../input.txt:
