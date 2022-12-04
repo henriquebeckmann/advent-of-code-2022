7796466298808ed2ffff1c3c8d6db76469af7210
Henrique Beckmann <henriquebeckmann2@gmail.com>
2022-12-03 21:20:05 -0300

    target folder removed

--- day01a/target/debug/deps/day01a-28ac58dfb6933833.d
+++
@@ -1,6 +0,0 @@
-/home/henrique/dev/git/advent-of-code-2022/day01a/target/debug/deps/day01a-28ac58dfb6933833: src/main.rs src/../input.txt
-
-/home/henrique/dev/git/advent-of-code-2022/day01a/target/debug/deps/day01a-28ac58dfb6933833.d: src/main.rs src/../input.txt
-
-src/main.rs:
-src/../input.txt:
--- day01a/target/debug/deps/day01a-719ac07cf9ffbe3f.d
+++
@@ -1,6 +0,0 @@
-/home/henrique/dev/git/advent-of-code-2022/day01a/target/debug/deps/day01a-719ac07cf9ffbe3f.rmeta: src/main.rs src/../input.txt
-
-/home/henrique/dev/git/advent-of-code-2022/day01a/target/debug/deps/day01a-719ac07cf9ffbe3f.d: src/main.rs src/../input.txt
-
-src/main.rs:
-src/../input.txt:
